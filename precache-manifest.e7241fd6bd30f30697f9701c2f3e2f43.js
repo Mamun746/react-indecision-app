13df6761d75250ec47b37ce3760de8c61b39b949
Mamun <abdullahalmamun914@gmail.com>
2019-01-07 22:20:07 +0600

    Updates

---
+++ precache-manifest.e7241fd6bd30f30697f9701c2f3e2f43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "852f98c8ba0d0870dd9a",
+    "url": "/react-indecision-app/static/js/runtime~main.852f98c8.js"
+  },
+  {
+    "revision": "95a0bf5784d2972ce35e",
+    "url": "/react-indecision-app/static/js/main.95a0bf57.chunk.js"
+  },
+  {
+    "revision": "847fae0b874cc9badee4",
+    "url": "/react-indecision-app/static/js/1.847fae0b.chunk.js"
+  },
+  {
+    "revision": "95a0bf5784d2972ce35e",
+    "url": "/react-indecision-app/static/css/main.2d452987.chunk.css"
+  },
+  {
+    "revision": "847fae0b874cc9badee4",
+    "url": "/react-indecision-app/static/css/1.0cb3d500.chunk.css"
+  },
+  {
+    "revision": "db3b02f734899fea0676a01a6c3d1c95",
+    "url": "/react-indecision-app/index.html"
+  }
+];
